43ee2c5113b42c976386325621cd06d77397539f
luispal7030 <i202224764@cibertec.edu.pe>
2023-06-08 18:51:59 -0500

    agregando header

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// todo: normalizacion de barra header para cambiar transparencia y color segun scrool
+
+const div = document.querySelector( 'header > div');
+
+
+console.log(div.innerHTML); // *consula al dom de la existencia de objetos dentro del div
+
