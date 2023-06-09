b2d95f81fdf93b526dcabdd65fece6600016151b
luispal7030 <i202224764@cibertec.edu.pe>
2023-06-08 19:21:10 -0500

    subida js scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-// todo: normalizacion de barra header para cambiar transparencia y color segun scrool
+// todo: normalizacion de barra header para cambiar transparencia y color segun scroll
 
 const div = document.querySelector( 'header > div');
-
+let prevY = window.scrollY;
 
 console.log(div.innerHTML); // *consula al dom de la existencia de objetos dentro del div
 
+window.addEventListener('scroll', function(){
+    prevY = window.scrollY;
+})
